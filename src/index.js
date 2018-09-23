081495255de9e67c86b86da71a7f0882a1425365
Misha Maniuk <mikhail.maniuk@gmail.com>
2018-09-23 15:50:17 +0300

    quadratic equation task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var arr = equation.split(' ');
+  var k = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === 'x^2') {
+      k.push(arr[i - 2]);
+      if(arr[i + 1] === '-') {
+        k.push(arr[i + 1] + arr[i + 2]);
+      } else {
+        k.push(arr[i + 2]);
+      }
+    }
+    if (arr[i] === 'x') {
+      if(arr[i + 1] === '-') {
+        k.push(arr[i + 1] + arr[i + 2]);
+      } else {
+        k.push(arr[i + 2]);
+      }
+    }
+  }
+
+  var a = k[0];
+  var b = k[1];
+  var c = k[2];
+  var d = (b*b) - (4 * a * c);
+  var x1 = Math.round((-(b) + Math.sqrt(d)) / (2 * a));
+  var x2 = Math.round((-(b) - Math.sqrt(d)) / (2 * a));
+
+  var kArr = [x1, x2];
+  kArr.sort(function(a , b){
+    return a - b;
+  })
+  
+  return kArr;
+
 }
